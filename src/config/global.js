222012c9bd26e95dc989393ce8aef39c7b25be73
Camilo <camilobr17@gmail.com>
2024-12-13 12:22:47 -0500

    Ajustes_repo

--- src/config/global.js
+++ src/config/global.js
@@ -155,7 +155,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228142_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
